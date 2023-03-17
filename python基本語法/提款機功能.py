8b04227ee26f10eaccbf72e0e559eefcf35f5d91
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-17 13:26:10 +0800

    Update 提款機功能.py

--- python基本語法/提款機功能.py
+++ python基本語法/提款機功能.py
@@ -10,33 +10,33 @@ class CreateBankAccount:
     def __init__(self, id, name):
         self.id = id
         self.name = name
-        self.balance = 0
+        self.balance = 0 #建立資料屬性
 
     
 
     def deposit(self, amount):
         if amount <= 0:
             print('必須要是正數')
-        self.balance += amount
+        self.balance += amount #存款
     
     
     def withdraw(self, amount): 
         if amount <= self.balance:
-            self.balance -= amount
+            self.balance -= amount 
         else:
-            print('餘額不足')
+            print('餘額不足')#提款
 
 
     def transfor(self,b):
         self.balance-=100
-        b.balance+=100    
+        b.balance+=100    #轉帳
 
-a = CreateBankAccount('123456', 'a') 
+a = CreateBankAccount('123456', 'a') #建立物件a
 a.deposit(300)
 a.withdraw(30)
 print(a.balance) 
 
-b = CreateBankAccount('789102', 'b') 
+b = CreateBankAccount('789102', 'b') #建立物件b
 b.deposit(200)
 b.withdraw(40)
 print(b.balance) 
@@ -45,7 +45,7 @@ print(b.balance)
     
 
 
-a.transfor(b)
+a.transfor(b) #a轉給b
 print(a.balance)
 print(b.balance) 
 
