770fa776b38a9c80d07ff32a5c41a9aca802cce3
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-17 13:04:20 +0800

    Update 圓餅圖.py

--- python基本語法/圓餅圖.py
+++ python基本語法/圓餅圖.py
@@ -10,5 +10,5 @@ import matplotlib.pyplot as plt
 sorts=['America','Australia','Japan','Europe','United Kingdom']
 fee=[10543,2105,1190,3346,980]
 
-plt.pie(fee,labels=sorts,explode=(0,0,0.3,0,0),autopct="%1.2f%%") #顯示項目的百分比小數點後兩位
+plt.pie(fee,labels=sorts,explode=(0,0,0.3,0,0),autopct="%1.2f%%") #項目三分離和顯示所有項目百分比小數點後兩位
 plt.show()
