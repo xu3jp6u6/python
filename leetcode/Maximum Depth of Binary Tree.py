8ad66b2cf12a57287ae5f0f7da5dfc135b6b9b95
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-18 21:58:24 +0800

    Update Maximum Depth of Binary Tree.py

--- leetcode/Maximum Depth of Binary Tree.py
+++ leetcode/Maximum Depth of Binary Tree.py
@@ -9,4 +9,7 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        return  max(self.maxDepth(root.left), self.maxDepth(root.right))+1
+        else:
+            left_depth = self.maxDepth(root.left)
+            right_depth = self.maxDepth(root.right)
+            return max(left_depth, right_depth) + 1
