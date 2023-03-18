d34f0cc54b58c29c45a4b2921ce3b997a156258a
xu3jp6u6 <127651151+xu3jp6u6@users.noreply.github.com>
2023-03-18 10:51:15 +0800

    Add files via upload

---
+++ leetcode/Intersection of Two Arrays.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 18 10:43:24 2023
+
+@author: USER
+"""
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        a=set(nums1)
+        b=set(nums2)
+        return a&b
